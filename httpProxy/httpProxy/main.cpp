7db0f61496be4ad64e0ca52b65c1a461cb5b94b5
Lukas Ferreira <unlisislukasferreira@hotmail.com>
2017-11-15 11:26:15 -0200

    Adding innitial code

---
+++ httpProxy/httpProxy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  httpProxy
+//
+//  Created by Lukas Ferreira on 15/11/2017.
+//  Copyright © 2017 Lukas Ferreira. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
